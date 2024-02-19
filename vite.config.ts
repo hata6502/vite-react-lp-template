d15231f0f687f78e0e0a5607ee628b488a3da91f
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2024-02-20 00:43:50 +0900

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.BASE ? process.env.BASE : "/vite-react-lp-template/",
+  });
+};
